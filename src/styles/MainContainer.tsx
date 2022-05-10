5226cf993b35dd518e091cd7f6ac9398cce50725
harunfr <harun-626@hotmail.com>
2022-05-10 14:49:26 +0300

    new layouts structure defined

--- src/styles/MainContainer.tsx
+++ src/styles/MainContainer.tsx
@@ -1,12 +1,5 @@
 import styled from 'styled-components/macro';
 
-const MainContainer = styled.div`
-  /* border: 2px solid red; */
-  /* min-width: 95vw; */
-  /* min-height: 99vh; */
-  display: grid;
-  gap: 1rem;
-  padding: 0 5rem;
-`;
+const MainContainer = styled.div``;
 
 export default MainContainer;
