34b341b82d51d89ae725bb2e65ed1496d777b625
harunfr <harun-626@hotmail.com>
2022-05-15 12:31:21 +0300

    added navigation styling

--- src/components/Navigation/styles.tsx
+++ src/components/Navigation/styles.tsx
@@ -1,20 +1,16 @@
 import styled from 'styled-components/macro';
 import { Link as ScrollLink } from 'react-scroll';
 
-import ItemWrapper from '../helpers/ItemWrapper';
-
 export const Container = styled.nav<{ isViewingHeader: boolean }>`
-  overflow: auto;
   position: sticky;
-  z-index: 3;
-  width: 100%;
+  max-width: 100%;
   top: 0;
-  /* border: 1px solid green; */
+  margin: 0 1rem;
   display: flex;
   justify-content: space-between;
   align-items: center;
   height: 3.875rem;
-  padding: 0 1rem;
+  /* padding-left: 1rem; */
   background-color: ${(props) =>
     props.isViewingHeader ? 'transparent' : '#ffffffe5'};
   border-bottom: ${(props) =>
@@ -22,36 +18,36 @@ export const Container = styled.nav<{ isViewingHeader: boolean }>`
   transition: background-color 0.5s ease-in;
 
   & a {
-    color: ${(props) => (props.isViewingHeader ? '#eee' : '#0AA0D0')};
+    color: ${(props) => (props.isViewingHeader ? '#ddd' : '#0AA0D0')};
+  }
+
+  & a:hover {
+    color: ${(props) => (props.isViewingHeader ? '#fff' : '#147191')};
   }
 `;
-export const Wrapper = styled(ItemWrapper)``;
-
-//
-// page text
-//
 
 export const Logo = styled.div`
-  // change this to img or smt.
   color: #eee;
-  /* padding-left: 1rem; */
 `;
 
 export const NavList = styled.ul`
   display: flex;
-  gap: 2rem;
   list-style: none;
 `;
 
-export const ListItem = styled.li``;
+export const ListItem = styled.li`
+  > * {
+    &:last-child {
+      padding-right: 0;
+    }
+  }
+`;
 export const Link = styled(ScrollLink)`
-  color: #eee;
   text-decoration: none;
-  border: none;
   font-family: 'Lato', Helvetica, sans-serif;
-  /* padding: 18px 14px; */
-  display: inline-block;
+  padding: 2rem;
   font-size: 0.875rem;
   font-weight: normal;
   text-transform: uppercase;
+  cursor: pointer;
 `;
